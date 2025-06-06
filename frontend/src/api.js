6598892d9ab64b87789995ceb4f9157119592128
eeulp <luispaulo.dsn@outlook.com>
2025-06-05 21:37:05 -0300

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = "https://carne.onrender.com";
 
 // Instância do Axios exportada para uso direto se necessário
 export const api = axios.create({
