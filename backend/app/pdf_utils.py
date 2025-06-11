b78a716bc9921c795ce1758dc673bc5b08cf6b5d
Luís Paulo <luispaulo.dsn@outlook.com>
2025-06-11 16:29:44 -0300

    jkk

--- backend/app/pdf_utils.py
+++ backend/app/pdf_utils.py
@@ -2,7 +2,8 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT, TA_LEFT
 from reportlab.lib.units import cm
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle, PageBreak, KeepTogether
-from reportlab.lib.colors import black, blue, red, HexColor, colors # Importar 'colors'
+from reportlab.lib import colors # Importa o módulo colors inteiro
+from reportlab.lib.colors import black, blue, red, HexColor
 from io import BytesIO
 from reportlab.graphics.barcode import qr
 from reportlab.graphics.shapes import Drawing
